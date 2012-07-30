e600bb1adbad052b8ea248383fc64ff470a5fe12
Jeremy Meredith <jsmeredith@gmail.com>
2012-07-30 15:13:42 -0400

    changing importer test case to print ALL meshes from a file.

--- test/testimport.cpp
+++ test/testimport.cpp
@@ -13,18 +13,22 @@
 #include "eavlExecutor.h"
 
 
-eavlDataSet *ReadWholeFile(const string &filename)
+eavlDataSet *ReadMeshFromFile(const string &filename, int meshindex)
 {
     eavlImporter *importer = eavlImporterFactory::GetImporterForFile(filename);
     
     if (!importer)
         THROW(eavlException,"Didn't determine proper file reader to use");
 
-    string mesh = importer->GetMeshList()[0];
-    eavlDataSet *out = importer->GetMesh(mesh, 0);
-    vector<string> allvars = importer->GetFieldList(mesh);
+    vector<string> allmeshes = importer->GetMeshList();
+    if (meshindex >= allmeshes.size())
+        return NULL;
+
+    string meshname = allmeshes[meshindex];
+    eavlDataSet *out = importer->GetMesh(meshname, 0);
+    vector<string> allvars = importer->GetFieldList(meshname);
     for (int i=0; i<allvars.size(); i++)
-        out->fields.push_back(importer->GetField(allvars[i], mesh, 0));
+        out->fields.push_back(importer->GetField(allvars[i], meshname, 0));
 
     return out;
 }
@@ -36,8 +40,16 @@ int main(int argc, char *argv[])
         if (argc != 2)
             THROW(eavlException,"Incorrect number of arguments");
 
-        eavlDataSet *data = ReadWholeFile(argv[1]);
-        data->PrintSummary(cout);
+        int meshindex = 0;
+        eavlDataSet *data = ReadMeshFromFile(argv[1], meshindex);
+        while (data)
+        {
+            data->PrintSummary(cout);
+            ++meshindex;
+            data = ReadMeshFromFile(argv[1], meshindex);
+            if (data)
+                cout << "\n\n";
+        }
     }
     catch (const eavlException &e)
     {
