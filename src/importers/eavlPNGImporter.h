4690ad650039c47b8ee36c03e4e94aacdf903240
Jeremy Meredith <jsmeredith@gmail.com>
2013-01-07 13:26:10 -0500

    adding PNG importer with no external dependencies.

---
+++ src/importers/eavlPNGImporter.h
@@ -0,0 +1,38 @@
+// Copyright 2010-2013 UT-Battelle, LLC.  See LICENSE.txt for more information.
+#ifndef EAVL_PNG_IMPORTER_H
+#define EAVL_PNG_IMPORTER_H
+
+#include "STL.h"
+#include "eavlDataSet.h"
+#include "eavlImporter.h"
+
+
+// ****************************************************************************
+// Class:  eavlPNGImporter
+//
+// Purpose:
+///   Import PNG files as regular grids.
+//
+// Programmer:  Jeremy Meredith
+// Creation:    January  7, 2013
+// ****************************************************************************
+class eavlPNGImporter : public eavlImporter
+{
+  public:
+    eavlPNGImporter(const string &filename);
+    ~eavlPNGImporter();
+
+    int                 GetNumChunks(const std::string &mesh) { return 1; }
+    vector<string>      GetFieldList(const std::string &mesh);
+    vector<string>      GetCellSetList(const std::string &mesh);
+
+    eavlDataSet   *GetMesh(const string &name, int chunk);
+    eavlField     *GetField(const string &name, const string &mesh, int chunk);
+
+  protected:
+    unsigned long width, height;
+    std::vector<unsigned char> rgba;
+};
+
+
+#endif
