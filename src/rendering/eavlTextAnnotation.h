e2773997279fc5dbaa1039b902634d10cfd2f746
Jeremy Meredith <jsmeredith@gmail.com>
2013-01-10 10:59:21 -0500

    cleanup of text annotation classes.

--- src/rendering/eavlTextAnnotation.h
+++ src/rendering/eavlTextAnnotation.h
@@ -22,61 +22,17 @@ class eavlTextAnnotation
 {
   protected:
     string text;
-    bool twod;
-    bool billboard;
-    bool fixed2Dscale;
-    float scale;
-    float x, y, z;
-    eavlMatrix4x4 mtx;
     eavlColor color;
   public:
-    eavlTextAnnotation(const string &txt, eavlColor c, float s,
-                       float ox, float oy)
+    eavlTextAnnotation(const string &txt, eavlColor c)
+        : text(txt), color(c)
     {
-        text = txt;
-        color = c;
-        scale = s;
-        x = ox;
-        y = oy;
-        z = 0;
-        twod = true;
-        billboard = false;
-        mtx.CreateIdentity();
     }
-    eavlTextAnnotation(const string &txt, eavlColor c, float s,
-                       float ox, float oy, float oz,
-                       bool scale_is_fixed_in_2D)
+    virtual void Render(eavlCamera &camera) = 0;
+  protected:
+    void RenderText(float scale)
     {
-        text = txt;
-        color = c;
-        scale = s;
-        x = ox;
-        y = oy;
-        z = oz;
-        twod = false;
-        billboard = true;
-        fixed2Dscale = scale_is_fixed_in_2D;
-        mtx.CreateIdentity();
-    }
-    eavlTextAnnotation(const string &txt, eavlColor c, float s,
-                       float ox, float oy, float oz,
-                       float nx, float ny, float nz,
-                       float ux, float uy, float uz)
-    {
-        text = txt;
-        color = c;
-        scale = s;
-        twod = false;
-        billboard = false;
-        x = y = z = 0;
-        mtx.CreateRBT(eavlPoint3(ox,oy,oz),
-                      eavlPoint3(ox,oy,oz) - eavlVector3(nx,ny,nz),
-                      eavlVector3(ux,uy,uz));
-
-    }
-
-    virtual void Render(eavlCamera &camera)
-    {
+        // set up a texture for the font if needed
         eavlBitmapFont *fnt = eavlBitmapFontFactory::GetDefaultFont();
         eavlTexture *tex = (eavlTexture*)fnt->userPointer;
         if (!tex)
@@ -95,78 +51,13 @@ class eavlTextAnnotation
             fnt->userPointer = tex;
         }
 
-        if (billboard)
-        {
-            if (fixed2Dscale)
-            {
-                eavlPoint3 p = camera.P * camera.V * eavlPoint3(x,y,z);
-
-                glMatrixMode( GL_PROJECTION );
-                glLoadIdentity();
-                glOrtho(-1,1, -1,1, -1,1);
-
-                glMatrixMode( GL_MODELVIEW );
-                glLoadIdentity();
-                mtx.CreateTranslate(p.x, p.y, -p.z);
-                glMultMatrixf(mtx.GetOpenGLMatrix4x4());
-                mtx.CreateScale(1./camera.aspect, 1, 1);
-                glMultMatrixf(mtx.GetOpenGLMatrix4x4());
-                // height given in (0,1) vert range, but we
-                // are currently in -1,1 range, so scale up by 2.0
-                mtx.CreateScale(2.0, 2.0, 1.0);
-                glMultMatrixf(mtx.GetOpenGLMatrix4x4());
-            }
-            else
-            {
-                glMatrixMode( GL_PROJECTION );
-                glLoadIdentity();
-                glMultMatrixf(camera.P.GetOpenGLMatrix4x4());
-
-                glMatrixMode( GL_MODELVIEW );
-                glLoadIdentity();
-                glMultMatrixf(camera.V.GetOpenGLMatrix4x4());
-
-                mtx.CreateRBT(eavlPoint3(x,y,z),
-                              eavlPoint3(x,y,z) - (camera.from-camera.at),
-                              camera.up);
-                glMultMatrixf(mtx.GetOpenGLMatrix4x4());
-
-            }
-        }
-        else if (twod)
-        {
-            glMatrixMode( GL_PROJECTION );
-            glLoadIdentity();
-            glOrtho(0,1, 0,1, -1,1);
-
-            glMatrixMode( GL_MODELVIEW );
-            glLoadIdentity();
-            mtx.CreateTranslate(x, y, 0);
-            glMultMatrixf(mtx.GetOpenGLMatrix4x4());
-            mtx.CreateScale(1./camera.aspect, 1, 1);
-            glMultMatrixf(mtx.GetOpenGLMatrix4x4());
-        }
-        else
-        {
-            glMatrixMode( GL_PROJECTION );
-            glLoadIdentity();
-            glMultMatrixf(camera.P.GetOpenGLMatrix4x4());
-
-            glMatrixMode( GL_MODELVIEW );
-            glLoadIdentity();
-            glMultMatrixf(camera.V.GetOpenGLMatrix4x4());
-
-            glMultMatrixf(mtx.GetOpenGLMatrix4x4());
-        }
-
-        glColor3fv(color.c);
-
-        //glTexEnvf( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE );
-
-        // Kerning causing overlapping polygons.  Ideally only draw pixels where alpha>0,
-        // though this causes problems for alpha between 0 and 1 (unless can solve with different blend func??).
-        // Simpler is to just disable depth test entirely, but then must draw all text last.
-        // Or maybe draw text with two passes, once to update Z (when alpha==1) and once to draw pixels (when alpha>0).
+        // Kerning causing overlapping polygons.  Ideally only draw
+        // pixels where alpha>0, though this causes problems for alpha
+        // between 0 and 1 (unless can solve with different blend
+        // func??).  Simpler is to just disable z-writing entirely,
+        // but then must draw all text last.  Or maybe draw text with
+        // two passes, once to update Z (when alpha==1) and once to
+        // draw pixels (when alpha>0).
         if (true)
         {
             glDepthMask(GL_FALSE);
@@ -182,6 +73,9 @@ class eavlTextAnnotation
         glEnable(GL_BLEND);
         glDisable(GL_LIGHTING);
         glTexEnvf(GL_TEXTURE_FILTER_CONTROL, GL_TEXTURE_LOD_BIAS, -.5);
+        //glTexEnvf( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE );
+
+        glColor3fv(color.c);
 
         glBegin(GL_QUADS);
 
@@ -220,6 +114,172 @@ class eavlTextAnnotation
         glDepthMask(GL_TRUE);
         glDisable(GL_ALPHA_TEST);
         tex->Disable();
+    }
+};
+
+// ****************************************************************************
+// Class:  eavlScreenTextAnnotation
+//
+// Purpose:
+///   Text location and height are in normalized screen space coordinates.
+//
+// Programmer:  Jeremy Meredith
+// Creation:    January 10, 2013
+//
+// Modifications:
+// ****************************************************************************
+class eavlScreenTextAnnotation : public eavlTextAnnotation
+{
+  protected:
+    float scale;
+    float x,y;
+  public:
+    eavlScreenTextAnnotation(const string &txt, eavlColor c, float s,
+                             float ox, float oy)
+        : eavlTextAnnotation(txt,c)
+    {
+        scale = s;
+        x = ox;
+        y = oy;
+    }
+    virtual void Render(eavlCamera &camera)
+    {
+        glMatrixMode( GL_PROJECTION );
+        glLoadIdentity();
+        glOrtho(0,1, 0,1, -1,1);
+
+        glMatrixMode( GL_MODELVIEW );
+        glLoadIdentity();
+
+        eavlMatrix4x4 mtx;
+        mtx.CreateTranslate(x, y, 0);
+        glMultMatrixf(mtx.GetOpenGLMatrix4x4());
+
+        mtx.CreateScale(1./camera.aspect, 1, 1);
+        glMultMatrixf(mtx.GetOpenGLMatrix4x4());
+
+        RenderText(scale);
+    }
+};
+
+// ****************************************************************************
+// Class:  eavlWorldTextAnnotation
+//
+// Purpose:
+///   Text location and height are in world space coordinates.
+//
+// Programmer:  Jeremy Meredith
+// Creation:    January 10, 2013
+//
+// Modifications:
+// ****************************************************************************
+class eavlWorldTextAnnotation : public eavlTextAnnotation
+{
+  protected:
+    float scale;
+    eavlMatrix4x4 mtx;
+  public:
+    eavlWorldTextAnnotation(const string &txt, eavlColor c, float s,
+                            float ox, float oy, float oz,
+                            float nx, float ny, float nz,
+                            float ux, float uy, float uz)
+        : eavlTextAnnotation(txt,c)
+    {
+        scale = s;
+        mtx.CreateRBT(eavlPoint3(ox,oy,oz),
+                      eavlPoint3(ox,oy,oz) - eavlVector3(nx,ny,nz),
+                      eavlVector3(ux,uy,uz));
+
+    }
+    virtual void Render(eavlCamera &camera)
+    {
+        glMatrixMode( GL_PROJECTION );
+        glLoadMatrixf(camera.P.GetOpenGLMatrix4x4());
+
+        glMatrixMode( GL_MODELVIEW );
+        glLoadMatrixf(camera.V.GetOpenGLMatrix4x4());
+
+        glMultMatrixf(mtx.GetOpenGLMatrix4x4());
+
+        RenderText(scale);
+    }
+};
+
+// ****************************************************************************
+// Class:  eavlBillboardTextAnnotation
+//
+// Purpose:
+///   Text location origin is in world space, but the text is rotated so it
+///   is always upright and always facing towards the user and upright.
+///   Height can either be in screen space height (so it doesn't change
+///   apparent size as camera moves), or in world space height (so it
+///   gets bigger and smaller based on distance to the viewer).
+//
+// Programmer:  Jeremy Meredith
+// Creation:    January 10, 2013
+//
+// Modifications:
+// ****************************************************************************
+class eavlBillboardTextAnnotation : public eavlTextAnnotation
+{
+  protected:
+    float scale;
+    float x,y,z;
+    bool fixed2Dscale;
+  public:
+    eavlBillboardTextAnnotation(const string &txt, eavlColor c, float s,
+                                float ox, float oy, float oz,
+                                bool scaleIsScreenSpace)
+        : eavlTextAnnotation(txt,c)
+    {
+        scale = s;
+        x = ox;
+        y = oy;
+        z = oz;
+        fixed2Dscale = scaleIsScreenSpace;
+    }
+    virtual void Render(eavlCamera &camera)
+    {
+        if (fixed2Dscale)
+        {
+            eavlPoint3 p = camera.P * camera.V * eavlPoint3(x,y,z);
+
+            glMatrixMode( GL_PROJECTION );
+            glLoadIdentity();
+            glOrtho(-1,1, -1,1, -1,1);
+
+            glMatrixMode( GL_MODELVIEW );
+            glLoadIdentity();
+
+            eavlMatrix4x4 mtx;
+            mtx.CreateTranslate(p.x, p.y, -p.z);
+            glMultMatrixf(mtx.GetOpenGLMatrix4x4());
+
+            mtx.CreateScale(1./camera.aspect, 1, 1);
+            glMultMatrixf(mtx.GetOpenGLMatrix4x4());
+
+            // height given in (0,1) vert range, but we
+            // are currently in -1,1 range, so scale up by 2.0
+            mtx.CreateScale(2.0, 2.0, 1.0);
+            glMultMatrixf(mtx.GetOpenGLMatrix4x4());
+        }
+        else
+        {
+            glMatrixMode( GL_PROJECTION );
+            glLoadMatrixf(camera.P.GetOpenGLMatrix4x4());
+
+            glMatrixMode( GL_MODELVIEW );
+            glLoadMatrixf(camera.V.GetOpenGLMatrix4x4());
+
+            eavlMatrix4x4 mtx;
+            mtx.CreateRBT(eavlPoint3(x,y,z),
+                          eavlPoint3(x,y,z) - (camera.from-camera.at),
+                          camera.up);
+            glMultMatrixf(mtx.GetOpenGLMatrix4x4());
+
+        }
+
+        RenderText(scale);
     }
 };
 
