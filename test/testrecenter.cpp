b3b40c64965aa3dea8a82c43e6b4758d7c0b0922
Jeremy Meredith <jsmeredith@gmail.com>
2013-07-26 17:19:53 -0400

    adding recenter test and adding recentering to normal test.

--- test/testrecenter.cpp
+++ test/testrecenter.cpp
@@ -9,8 +9,6 @@
 #include "eavlImporterFactory.h"
 #include "eavlVTKExporter.h"
 
-#include "eavlExternalFaceMutator.h"
-#include "eavlSurfaceNormalMutator.h"
 #include "eavlCellToNodeRecenterMutator.h"
 #include "eavlExecutor.h"
 
@@ -49,66 +47,28 @@ int main(int argc, char *argv[])
         eavlExecutor::SetExecutionMode(eavlExecutor::PreferGPU);
         eavlInitializeGPU();
 
-        if (argc != 2 && argc != 3)
+        if (argc != 3 && argc != 4)
             THROW(eavlException,"Incorrect number of arguments");
 
         // Read the input
         eavlDataSet *data = ReadWholeFile(argv[1]);
 
-        int cellsetindex = -1;
-        for (int i=0; i<data->GetNumCellSets(); i++)
-        {
-            if (data->GetCellSet(i)->GetDimensionality() == 2)
-            {
-                cellsetindex = i;
-                cerr << "Found 2D topo dim cell set name '"
-                     << data->GetCellSet(i)->GetName()
-                     << "' index " << cellsetindex << endl;
-                break;
-            }
-        }
-        if (cellsetindex < 0)
-        {
-            cerr << "Couldn't find a 2D cell set.  Trying to add external faces to a 3D set.\n";
-            int cellsetindex3d = -1;
-            for (int i=0; i<data->GetNumCellSets(); i++)
-            {
-                if (data->GetCellSet(i)->GetDimensionality() == 3)
-                {
-                    cellsetindex3d = i;
-                    cerr << "Found 3D topo dim cell set index "<<cellsetindex3d<<endl;
-                    break;
-                }
-            }
-            if (cellsetindex3d < 0)
-                THROW(eavlException,"Couldn't find a 3D set to add faces, to either.  Aborting.");
+        eavlField *f = data->GetField(argv[2]);
+        if (f->GetAssociation() != eavlField::ASSOC_CELL_SET)
+            THROW(eavlException, "Wanted a cell-centered field.");
 
-            eavlExternalFaceMutator *extface = new eavlExternalFaceMutator;
-            extface->SetDataSet(data);
-            extface->SetCellSet(data->GetCellSet(cellsetindex3d)->GetName());
-            extface->Execute();
-            delete extface;
-            cellsetindex = data->GetNumCellSets() - 1;
-        }
-
-        if (data->GetCoordinateSystem(0)->GetDimension() != 3)
-            THROW(eavlException,"Not 3D coords.  Want 3D coords for now.\n");
-
-        eavlSurfaceNormalMutator *surfnorm = new eavlSurfaceNormalMutator;
-        surfnorm->SetDataSet(data);
-        surfnorm->SetCellSet(data->GetCellSet(cellsetindex)->GetName());
-        surfnorm->Execute();
+        int cellsetindex = f->GetAssocCellSet();
 
         eavlCellToNodeRecenterMutator *cell2node = new eavlCellToNodeRecenterMutator;
         cell2node->SetDataSet(data);
-        cell2node->SetField("surface_normals");
+        cell2node->SetField(argv[2]);
         cell2node->SetCellSet(data->GetCellSet(cellsetindex)->GetName());
         cell2node->Execute();
 
-        if (argc == 3)
+        if (argc == 4)
         {
             cerr << "\n\n-- done with surface normal, writing to file --\n";	
-            WriteToVTKFile(data, argv[2], cellsetindex);
+            WriteToVTKFile(data, argv[3], cellsetindex);
         }
         else
         {
@@ -122,9 +82,10 @@ int main(int argc, char *argv[])
     catch (const eavlException &e)
     {
         cerr << e.GetErrorText() << endl;
-        cerr << "\nUsage: "<<argv[0]<<" <infile.vtk> [<outfile.vtk>]\n";
+        cerr << "\nUsage: "<<argv[0]<<" <infile.vtk> <fieldname> [<outfile.vtk>]\n";
         return 1;
     }
+
 
     eavlTimer::Dump(cout);
 
