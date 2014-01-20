9ef2ce66129f64b6d3c6a529ba3bf897fcb77c40
Jeremy Meredith <jsmeredith@gmail.com>
2014-01-19 21:28:00 -0500

    adding an option to testiso to generate sample data instead of loading from a file

--- test/testiso.cpp
+++ test/testiso.cpp
@@ -12,6 +12,123 @@
 #include "eavlIsosurfaceFilter.h"
 #include "eavlExecutor.h"
 
+#include "eavlCellSetExplicit.h"
+#include "eavlCellSetAllStructured.h"
+#include "eavlCoordinates.h"
+#include "eavlLogicalStructureRegular.h"
+
+
+eavlDataSet *GenerateRectXY(int ni, int nj)
+{
+    eavlDataSet *data = new eavlDataSet();
+
+    // set the number of points
+    int npts = ni * nj;
+    data->SetNumPoints(npts);
+
+    eavlRegularStructure reg;
+    reg.SetNodeDimension2D(ni, nj);
+
+    // set the logical structure
+    eavlLogicalStructure *log = new eavlLogicalStructureRegular(reg.dimension,
+                                                                reg);
+    data->SetLogicalStructure(log);
+
+    // create the coordinate axes
+    eavlFloatArray *x = new eavlFloatArray("x", 1, ni);
+    for (int i=0; i<ni; ++i)
+        x->SetValue(i, float(i)/float(ni-1));
+
+    eavlFloatArray *y = new eavlFloatArray("y", 1, nj);
+    for (int j=0; j<nj; ++j)
+        y->SetValue(j, float(j)/float(nj-1));
+
+    // add the coordinate axis arrays as linear fields on logical dims
+    data->AddField(new eavlField(1, x, eavlField::ASSOC_LOGICALDIM, 0));
+    data->AddField(new eavlField(1, y, eavlField::ASSOC_LOGICALDIM, 1));
+
+    // create a radial nodal field
+    eavlFloatArray *rad = new eavlFloatArray("rad", 1, npts);
+    for (int j=0; j<nj; ++j)
+        for (int i=0; i<ni; ++i)
+            rad->SetValue(j*ni + i, i*i + j*j);
+    data->AddField(new eavlField(1,rad,eavlField::ASSOC_POINTS));
+
+    // set the coordinates
+    eavlCoordinates *coords = new eavlCoordinatesCartesian(log,
+                                            eavlCoordinatesCartesian::X,
+                                            eavlCoordinatesCartesian::Y);
+    coords->SetAxis(0, new eavlCoordinateAxisField("x"));
+    coords->SetAxis(1, new eavlCoordinateAxisField("y"));
+    data->AddCoordinateSystem(coords);
+
+    // create a cell set implicitly covering the entire regular structure
+    eavlCellSet *cells = new eavlCellSetAllStructured("cells", reg);
+    data->AddCellSet(cells);
+
+    return data;
+}
+
+
+eavlDataSet *GenerateRectXYZ(int ni, int nj, int nk)
+{
+    eavlDataSet *data = new eavlDataSet();
+
+    // set the number of points
+    int npts = ni * nj * nk;
+    data->SetNumPoints(npts);
+
+    eavlRegularStructure reg;
+    reg.SetNodeDimension3D(ni, nj, nk);
+
+    // set the logical structure
+    eavlLogicalStructure *log = new eavlLogicalStructureRegular(reg.dimension,
+                                                                reg);
+    data->SetLogicalStructure(log);
+
+    // create the coordinate axes
+    eavlFloatArray *x = new eavlFloatArray("x", 1, ni);
+    for (int i=0; i<ni; ++i)
+        x->SetValue(i, float(i)/float(ni-1));
+
+    eavlFloatArray *y = new eavlFloatArray("y", 1, nj);
+    for (int j=0; j<nj; ++j)
+        y->SetValue(j, float(j)/float(nj-1));
+
+    eavlFloatArray *z = new eavlFloatArray("z", 1, nk);
+    for (int k=0; k<nk; ++k)
+        z->SetValue(k, float(k)/float(nk-1));
+
+    // add the coordinate axis arrays as linear fields on logical dims
+    data->AddField(new eavlField(1, x, eavlField::ASSOC_LOGICALDIM, 0));
+    data->AddField(new eavlField(1, y, eavlField::ASSOC_LOGICALDIM, 1));
+    data->AddField(new eavlField(1, z, eavlField::ASSOC_LOGICALDIM, 2));
+
+    // create a radial nodal field
+    eavlFloatArray *rad = new eavlFloatArray("rad", 1, npts);
+    for (int k=0; k<nk; ++k)
+        for (int j=0; j<nj; ++j)
+            for (int i=0; i<ni; ++i)
+                rad->SetValue(k*ni*nj + j*ni + i, i*i + j*j + k*k);
+    data->AddField(new eavlField(1,rad,eavlField::ASSOC_POINTS));
+
+    // set the coordinates
+    eavlCoordinates *coords = new eavlCoordinatesCartesian(log,
+                                            eavlCoordinatesCartesian::X,
+                                            eavlCoordinatesCartesian::Y,
+                                            eavlCoordinatesCartesian::Z);
+    coords->SetAxis(0, new eavlCoordinateAxisField("x"));
+    coords->SetAxis(1, new eavlCoordinateAxisField("y"));
+    coords->SetAxis(2, new eavlCoordinateAxisField("z"));
+    data->AddCoordinateSystem(coords);
+
+    // create a cell set implicitly covering the entire regular structure
+    eavlCellSet *cells = new eavlCellSetAllStructured("cells", reg);
+    data->AddCellSet(cells);
+
+    return data;
+}
+
 
 eavlDataSet *ReadWholeFile(const string &filename)
 {
@@ -46,7 +163,8 @@ int main(int argc, char *argv[])
     {   
         eavlInitializeGPU();
 
-        if (argc != 4 && argc != 5)
+        if (argc != 4 && argc != 5 &&
+            argc != 6 && argc != 7)
             THROW(eavlException,"Incorrect number of arguments");
 
         char *tmp;
@@ -54,12 +172,36 @@ int main(int argc, char *argv[])
         if (tmp == argv[1])
             THROW(eavlException,"Expected a value for first argument");
 
+        const char *outfile = NULL;
+
+        if (argc == 5)
+            outfile = argv[4];
+        else if (argc == 7)
+            outfile = argv[6];
+
         const char *fieldname = argv[2];
     
         // Read the input
-        eavlDataSet *data = ReadWholeFile(argv[3]);
+        eavlDataSet *data = NULL;
+        if (argc == 4 || argc == 5)
+        {
+            data = ReadWholeFile(argv[3]);
+        }
+        else
+        {
+            int ni = atoi(argv[3]);
+            int nj = atoi(argv[4]);
+            int nk = atoi(argv[5]);
+            if (ni<2 || nj<2 || nk<2)
+                THROW(eavlException, "Expected ni,nj,nk >= 2");
+            //eavlDataSet *data = GenerateRectXY(ni,nj);
+            data = GenerateRectXYZ(ni,nj,nk);
+        }
+
         //cout << "\n\n-- summary of data set input --\n";	
         //data->PrintSummary(cout);
+
+        //WriteToVTKFile(data, "test.vtk", 0);
 
         int cellsetindex = -1;
         for (int i=0; i<data->GetNumCellSets(); i++)
@@ -97,10 +239,10 @@ int main(int argc, char *argv[])
         // For debugging we can add some temp arrays to input data set
         //WriteToVTKFile(data, "input_modified.vtk", 0);
 
-        if (argc == 5)
+        if (outfile)
         {
             cerr << "\n\n-- done isosurfacing, writing to file --\n";	
-            WriteToVTKFile(iso->GetOutput(), argv[4], 0);
+            WriteToVTKFile(iso->GetOutput(), outfile, 0);
         }
         else
         {
@@ -115,6 +257,7 @@ int main(int argc, char *argv[])
     {
         cerr << e.GetErrorText() << endl;
         cerr << "\nUsage: "<<argv[0]<<" <value> <fieldname> <infile.vtk> [<outfile.vtk>]\n";
+        cerr << "        "<<argv[0]<<" <value> <fieldname> <nx> <ny> <nz> [<outfile.vtk>]\n";
         return 1;
     }
 
