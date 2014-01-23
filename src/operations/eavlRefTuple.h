48dbaf31afb55f1cf91130e7a8665d06d2fa6b85
Jeremy Meredith <jsmeredith@gmail.com>
2014-01-23 14:28:17 -0500

    one tiny addition to 16-length tuple support.

--- src/operations/eavlRefTuple.h
+++ src/operations/eavlRefTuple.h
@@ -25,7 +25,7 @@ struct refcons
     
     template <class T0, class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9, class T10, class T11, class T12, class T13, class T14, class T15>
     EAVL_HOSTDEVICE refcons(T0 &t0, T1 &t1, T2 &t2, T3 &t3, T4 &t4, T5 &t5, T6 &t6, T7 &t7, T8 &t8, T9 &t9, T10 &t10, T11 &t11, T12 &t12, T13 &t13, T14 &t14, T15 &t15)
-        : first(t0), rest(t1,t2,t3,t4,t5,t6,t7, cnull())
+        : first(t0), rest(t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14,t15, cnull())
     {
     }
 
