3bd80a3357fab77cff162b1b41f113b9788b4c1b
Jeremy Meredith <jsmeredith@gmail.com>
2014-08-12 17:15:15 -0400

    check cell points against threshold range, choose between all or some.

--- src/filters/eavlThresholdMutator.cpp
+++ src/filters/eavlThresholdMutator.cpp
@@ -7,6 +7,9 @@
 
 eavlThresholdMutator::eavlThresholdMutator()
 {
+    minval = -FLT_MAX;
+    minval = +FLT_MAX;
+    all_points_required = false;
 }
 
 
@@ -18,11 +21,10 @@ eavlThresholdMutator::Execute()
 
     eavlField   *inField = dataset->GetField(fieldname);
 
-    ///\todo: add nodal threshold capability
     eavlField::Association fieldAssociation = inField->GetAssociation();
-	if(fieldAssociation != eavlField::ASSOC_POINTS &&
-		(inField->GetAssociation() != eavlField::ASSOC_CELL_SET ||
-        inField->GetAssocCellSet() != dataset->GetCellSet(inCellSetIndex)->GetName()))
+    if (fieldAssociation != eavlField::ASSOC_POINTS &&
+        (inField->GetAssociation() != eavlField::ASSOC_CELL_SET ||
+         inField->GetAssocCellSet() != dataset->GetCellSet(inCellSetIndex)->GetName()))
     {
         THROW(eavlException,"Field for subset didn't match cell set.");
     }
@@ -31,53 +33,67 @@ eavlThresholdMutator::Execute()
 
     vector<int> newcells;
     int in_ncells = inCells->GetNumCells();
-    for (int i=0; i<in_ncells; i++)
+    if (fieldAssociation == eavlField::ASSOC_CELL_SET)
     {
-        if (inArray->GetComponentAsDouble(i,0) >= minval &&
-            inArray->GetComponentAsDouble(i,0) <= maxval)
+        for (int i=0; i<in_ncells; i++)
         {
-            newcells.push_back(i);
-        }            
+            if (inArray->GetComponentAsDouble(i,0) >= minval &&
+                inArray->GetComponentAsDouble(i,0) <= maxval)
+            {
+                newcells.push_back(i);
+            }            
+        }
+    }
+    else // (fieldAssociation == eavlField::ASSOC_POINTS)
+    {
+        for (int i=0; i<in_ncells; i++)
+        {
+            bool all_in = true;
+            bool some_in = false;
+            eavlCell cell = inCells->GetCellNodes(i);
+            for (int j=0; j<cell.numIndices; j++)
+            {
+                double val = inArray->GetComponentAsDouble(cell.indices[j],0);
+                if (val >= minval && val <= maxval)
+                    some_in = true;
+                else
+                    all_in = false;
+            }
+
+            if (all_points_required)
+            {
+                if (all_in)
+                    newcells.push_back(i);
+            }
+            else
+            {
+                if (some_in)
+                    newcells.push_back(i);
+            }
+        }
     }
     unsigned int numnewcells = newcells.size();
-	
-	string subsetName;
-    if(fieldAssociation == eavlField::ASSOC_CELL_SET) 
+
+    eavlExplicitConnectivity conn;
+    for (unsigned int i=0; i<numnewcells; ++i)
     {
-    	eavlExplicitConnectivity conn;
-	    for (unsigned int i=0; i<numnewcells; ++i)
-    	{
-    	    eavlCell cell = inCells->GetCellNodes(newcells[i]);
-    	    conn.AddElement(cell);
-    	}
-	
-    	eavlCellSetExplicit *subset = new eavlCellSetExplicit(
-    										string("threshold_of_")+inCells->GetName(),
-                                            inCells->GetDimensionality()
-                                            );
-    	subset->SetCellNodeConnectivity(conn);
-    	dataset->AddCellSet(subset);
-    	subsetName = subset->GetName();
-    }
-    else 
-    {
-    	eavlCellSetAllPoints *subset = new eavlCellSetAllPoints(
-    										string("threshold_of_")+inCells->GetName(), 
-    										numnewcells
-    										);
-		dataset->AddCellSet(subset);
-		subsetName = subset->GetName();
+        eavlCell cell = inCells->GetCellNodes(newcells[i]);
+        conn.AddElement(cell);
     }
 
+    eavlCellSetExplicit *subset = new eavlCellSetExplicit(string("threshold_of_")+inCells->GetName(),
+                                                          inCells->GetDimensionality());
+    subset->SetCellNodeConnectivity(conn);
 
-    int numDatasetFields = dataset->GetNumFields();
-    for (int i=0; i<numDatasetFields; i++)
+    //int new_cellset_index = dataset->GetNumCellSets();
+    dataset->AddCellSet(subset);
+
+    for (int i=0; i<dataset->GetNumFields(); i++)
     {
         eavlField *f = dataset->GetField(i);
-        if(fieldAssociation == eavlField::ASSOC_POINTS ||
-			(inField->GetAssociation() == eavlField::ASSOC_CELL_SET &&
-    	     inField->GetAssocCellSet() == dataset->GetCellSet(inCellSetIndex)->GetName()))
-    	{
+        if (f->GetAssociation() == eavlField::ASSOC_CELL_SET &&
+            f->GetAssocCellSet() == dataset->GetCellSet(inCellSetIndex)->GetName())
+        {
             int numcomp = f->GetArray()->GetNumberOfComponents();
             eavlFloatArray *a = new eavlFloatArray(
                                  string("subset_of_")+f->GetArray()->GetName(),
@@ -90,8 +106,8 @@ eavlThresholdMutator::Execute()
             }
 
             eavlField *newfield = new eavlField(f->GetOrder(), a,
-                                                fieldAssociation,
-                                                subsetName);
+                                                eavlField::ASSOC_CELL_SET,
+                                                subset->GetName());
             dataset->AddField(newfield);
         }
     }
